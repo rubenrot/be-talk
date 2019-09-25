8bd294321fc1c62285a605ecd656ddde97e0bfb8
Rubén Rot <ruben.rot.next@bbva.com>
2019-09-25 18:54:03 +0200

    Cuentas

--- app/elements/data-managers/position-dm/position-dm.js
+++ app/elements/data-managers/position-dm/position-dm.js
@@ -95,7 +95,7 @@
         'x-jwt-auth': token
       };
 
-      if(window.location.hostname === 'localhost'){
+      if (window.location.hostname === 'localhost') {
         this.$.accountsDP.method = 'GET';
       } else {
         this.$.accountsDP.method = 'POST';
@@ -108,8 +108,10 @@
     }
 
     _onRequestOkAccounts(evt) {
-      const accounts = evt.detail.cuentas;
-      this.accounts = Object.assign([], accounts);
+      const accounts = evt.detail.positions;
+
+      console.log('Construcción de la cuentas');
+      this.accounts = Object.assign([], this.getDashAccounts(accounts));
       /* this.dispatchEvent(new CustomEvent('load-accounts',
          {
            detail:
@@ -178,7 +180,6 @@
     _onRequestOkAccountDetail(evt) {
       const movimientos = evt.detail.movimientos;
       this.movements = Object.assign([], movimientos);
-      console.log(movimientos);
       //console.log(data);
 
       this.dispatchEvent(new CustomEvent('load-account-detail',
@@ -204,6 +205,81 @@
         customerId: accountId
       };
       this.$.accountDetailDP.generateRequest();
+    }
+
+
+    getDashAccounts(objectResponseASO) {
+      var me = this;
+      var accounts = [];
+      objectResponseASO.forEach(function (position) {
+        if (position.balanceAgrupation === 'Account') {
+          accounts.push(me.getJSONAccountMapped(position));
+        }
+      });
+
+      return accounts;
+    }
+
+    getJSONAccountMapped(item, processCode, serviceCode, configApp) {
+      var positionAccountObject;
+
+      var itemContract = this.checkValueDefined(item.contract.account, item.contract);
+
+      //Rellenamos la informacion de la posicion de la cuenta
+      positionAccountObject = this.generatePositionInvestmentInfoAccount(
+        item, itemContract.formats.iban, 'Cuenta',
+        this.checkValueDefinedComplex(itemContract.currentBalanceLocalCurrency, 'amount', 0),
+        this.checkValueDefinedComplex(itemContract.currentBalanceLocalCurrency, 'currency', 'EUR'),
+        this.checkValueDefinedComplex(itemContract.availableBalance, 'amount', 0),
+        this.checkValueDefinedComplex(itemContract.availableBalance, 'currency', 'EUR'));
+
+      return positionAccountObject;
+
+    }
+
+    checkValueDefined(valueDefined, valueAlternative) {
+      return (undefined !== valueDefined ? valueDefined : valueAlternative);
+    }
+
+    checkValueDefinedComplex(valueDefined, returnValueDefined, returnValueAlternative) {
+      return (undefined !== valueDefined ? (undefined !== Object.getOwnPropertyDescriptor(valueDefined, returnValueDefined) ? Object.getOwnPropertyDescriptor(valueDefined, returnValueDefined).value : returnValueAlternative) : returnValueAlternative);
+    }
+
+    generatePositionInvestmentInfoAccount(position, contractDescription, contractType, currentAmount, currentCurrency, avaliableAmount, avaliableCurrency) {
+      position = this.generatePositionInvestmentInfo(position, contractDescription, contractType, currentAmount, currentCurrency);
+      position.contract.availableBalance = {};
+      position.contract.availableBalance.amount = avaliableAmount;
+      position.contract.availableBalance.currency = {};
+      if (avaliableCurrency.id === undefined) {
+        position.contract.availableBalance.currency.id = avaliableCurrency;
+      } else {
+        position.contract.availableBalance.currency.id = avaliableCurrency.id;
+      }
+      return position;
+    }
+
+    generatePositionInvestmentInfo(position, contractDescription, contractType, amount, currency, processCode, serviceCode, configApp, dueDate) {
+      if (undefined === position.contract) {
+        position.contract = {};
+      }
+      position.contract.contractDescription = contractDescription;
+      position.contract.contractType = contractType;
+      position.contract.balance = {};
+      position.contract.balance.amount = amount;
+      position.contract.balance.currency = {};
+      if (currency.id === undefined) {
+        position.contract.balance.currency.id = currency;
+      } else {
+        position.contract.balance.currency.id = currency.id;
+      }
+      //Indicador de orden
+      position.contract.defaultOrderIndex2 = contractDescription;
+      position.processCode = processCode;
+      position.serviceCode = serviceCode;
+      if (dueDate !== undefined) {
+        position.contract.dueDate = dueDate;
+      }
+      return position;
     }
 
 
